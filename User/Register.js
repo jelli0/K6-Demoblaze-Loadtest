eb2a510186ba71d081326e15f092d70edde69f31
Jelly <jellyperwita@gmail.com>
2025-08-14 15:49:38 +0700

    update

--- User/Register.js
+++ User/Register.js
@@ -4,24 +4,29 @@ import { getHomepage } from '../Basic.js';
 
 
 export const options = {
-  vus: 10,      // Virtual Users
-  maxDuration: 5, // durasi test
+  vus: 5,     // Virtual Users
+  iterations: 10,
 };
 
 export default function () {
     // Generate username & password
     let timestamp = Date.now();
-    let username = `user_${timestamp}`;
-    let password = `pass_${timestamp}`;
+    let random = Math.floor(Math.random() * 100);
+    let username = `user_${timestamp}_${random}`;
+    let password = `pass_${timestamp}_${random}`;
 
-    let payload = JSON.stringify({
+    const payload = JSON.stringify({
         username: username,
         password: password
     });
 
-    let jelly = http.post('https://api.demoblaze.com/signup', payload, {
-        headers: { 'Content-Type': 'application/json' },
-    });
+    const params = {
+    headers: {
+      'Content-Type': 'application/json'
+    }
+    };
+
+    let jelly = http.post('https://api.demoblaze.com/signup', payload, params);
 
     // Validasi response
     check(jelly, {
